1bd40943beb3345efa785e8bdb3c5935fddc19c6
Alkim Mert Pala <service@jummix.com>
2025-08-18 00:22:25 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -27,9 +27,9 @@ const nextConfig = {
   experimental: {
     // This is required to allow the Next.js dev server to be accessed from
     // a different domain, which is the case in cloud development environments.
-    // allowedDevOrigins: [
-    //   'https://6000-firebase-studio-1755331739303.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev',
-    // ],
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1755331739303.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev',
+    ],
   },
 };
 
