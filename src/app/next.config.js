7dff54b51a4a1d5a96cda9e157add4d86e958bb1
Alkim Mert Pala <service@jummix.com>
2025-09-20 08:47:49 +0000

    ok orientiere dich nun auch in der eventdetailseite an diesen webseites

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -22,16 +22,11 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
 
 module.exports = nextConfig;
 
+    
     
--- src/components/jummix/EventDetailClient.tsx
+++ src/components/jummix/EventDetailClient.tsx
@@ -421,3 +421,5 @@ export function EventDetailClient({ event }: EventDetailClientProps) {
     </div>
   );
 }
+
+    
